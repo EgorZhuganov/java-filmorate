0733bc74d4b484ec1b3fbad87f7ae78e61541af3
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-22 11:24:26 +1000

    test: добавить тест для UserReadMapper и UserUpdateMapper

---
+++ src/test/java/ru/yandex/practicum/filmorate/mapper/userMapper/UserReadMapperTest.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.mapper.userMapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.dto.userDto.UserCreateDto;
+import ru.yandex.practicum.filmorate.dto.userDto.UserReadDto;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserReadMapperTest {
+
+    UserReadMapper mapper = new UserReadMapper();
+    private final User user = User
+            .builder()
+            .id(1L)
+            .email("myMail@mail.ru")
+            .login("myLogin")
+            .displayName("myDisplayName")
+            .birthday(LocalDate.of(2000, 1, 1))
+            .build();
+
+    @Test
+    void mapFrom() {
+        UserReadDto userDto = mapper.mapFrom(user);
+        Assertions.assertEquals(user.getId(), userDto.getId());
+        Assertions.assertEquals(user.getEmail(), userDto.getEmail());
+        Assertions.assertEquals(user.getLogin(), userDto.getLogin());
+        Assertions.assertEquals(user.getDisplayName(), userDto.getDisplayName());
+        Assertions.assertEquals(user.getBirthday(), userDto.getBirthday());
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/mapper/userMapper/UserUpdateMapperTest.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.filmorate.mapper.userMapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.dto.userDto.UserReadDto;
+import ru.yandex.practicum.filmorate.dto.userDto.UserUpdateDto;
+import ru.yandex.practicum.filmorate.mapper.Mapper;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserUpdateMapperTest {
+
+    UserUpdateMapper mapper = new UserUpdateMapper();
+    private final User user = User
+            .builder()
+            .id(1L)
+            .email("myMail@mail.ru")
+            .login("myLogin")
+            .displayName("myDisplayName")
+            .birthday(LocalDate.of(2000, 1, 1))
+            .build();
+
+    private final UserUpdateDto userUpdateDto = new UserUpdateDto(1L, "ya@mail.ru", "myLogin",
+            "myDisplayName", LocalDate.of(1984,12,12));
+
+    @Test
+    void mapFrom() {
+        User updatedUser = mapper.mapFrom(userUpdateDto, user);
+
+        Assertions.assertEquals(userUpdateDto.getId(), updatedUser.getId());
+        Assertions.assertEquals(userUpdateDto.getEmail(), updatedUser.getEmail());
+        Assertions.assertEquals(userUpdateDto.getLogin(), updatedUser.getLogin());
+        Assertions.assertEquals(userUpdateDto.getDisplayName(), updatedUser.getDisplayName());
+        Assertions.assertEquals(userUpdateDto.getBirthday(), updatedUser.getBirthday());
+    }
+}
