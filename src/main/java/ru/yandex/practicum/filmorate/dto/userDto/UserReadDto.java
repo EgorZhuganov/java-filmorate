5b6f543a37fd528974c8b85d520bf0afcbe2e5b1
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-22 10:49:29 +1000

    change: изменить UserReadDto

--- src/main/java/ru/yandex/practicum/filmorate/dto/userDto/UserReadDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/userDto/UserReadDto.java
@@ -1,28 +1,16 @@
 package ru.yandex.practicum.filmorate.dto.userDto;
 
 import lombok.*;
-import lombok.experimental.FieldDefaults;
 
-import javax.validation.constraints.*;
 import java.time.LocalDate;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@Value
 public class UserReadDto {
 
-    @NotNull
     Long id;
-    @Email
     String email;
-    @NotBlank @Size(min = 3, max = 10)
     String login; //TODO подумать об удалении после спринта
-    @NotNull
     String displayName;
-    @PastOrPresent
     LocalDate birthday;
 
 }
