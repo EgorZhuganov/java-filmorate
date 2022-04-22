b8d3c2fdfc4de5989cf84793ee101d4f2be997d3
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-22 13:52:05 +1000

    change: удалить валидацию на текущем уровне, заиспользовать аннотацию ResponseBody

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,65 +9,43 @@ import ru.yandex.practicum.filmorate.dto.userDto.UserReadDto;
 import ru.yandex.practicum.filmorate.dto.userDto.UserUpdateDto;
 import ru.yandex.practicum.filmorate.service.UserService;
 
-import javax.validation.Valid;
+import java.util.List;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping //200 OK and return List
-    public String findAll() {
-        String userListAsJson = null;
-        try {
-            userListAsJson = new ObjectMapper().writeValueAsString(userService.findAll());
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return userListAsJson;
+    @ResponseBody
+    public List<UserReadDto> findAll() {
+        return userService.findAll();
     }
 
     @GetMapping("/{id}") //200, OK orElseThrow NOT_FOUND
-    public String findById(@PathVariable Long id) {
-        String userAsJson = null;
-        try {
-            userAsJson = new ObjectMapper().writeValueAsString(userService
-                    .findById(id) //TODO что возвращает и в каком виде?
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return userAsJson;
+    @ResponseBody
+    public UserReadDto findById(@PathVariable Long id) {
+        return userService
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping //201 and return userReadDto
     @ResponseStatus(HttpStatus.CREATED)
-    public String create(@Valid @RequestBody UserCreateDto user) {
-        UserReadDto userDto = userService.create(user);
-        String userAsJson;
-        try {
-            userAsJson = new ObjectMapper().writeValueAsString(userDto);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return userAsJson;
+    @ResponseBody
+    public UserReadDto create(@RequestBody UserCreateDto user) {
+        return userService.create(user);
     }
 
     @PutMapping //204 no content
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String update(@Valid @RequestBody UserUpdateDto user) {
-        UserReadDto userDto = userService
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public UserReadDto update(@RequestBody UserUpdateDto user) {
+        return userService
                 .update(user.getId(), user)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        String userAsJson;
-        try {
-            userAsJson = new ObjectMapper().writeValueAsString(userDto);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return userAsJson;
     }
 
     @DeleteMapping("/{id}") //204 no content
