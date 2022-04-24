69d763e522dc6fb8fbdbf139573acf207c42ac49
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-24 18:22:55 +1000

    fix: изменить сообщение для аннотации IsAfter

--- src/main/java/ru/yandex/practicum/filmorate/validation/annotation/IsAfter.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/annotation/IsAfter.java
@@ -8,15 +8,22 @@ import java.lang.annotation.*;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target(ElementType.FIELD)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RUNTIME)
 @Constraint(validatedBy = DateValidator.class)
 @Documented
 public @interface IsAfter{
-    String message() default "date have to be after then \"${validatedValue}\"";
+    String message() default "дата должна быть позже чем \"${validatedValue}\"";
     String minDate();
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
+    @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    public @interface List {
+        IsAfter[] value();
+    }
 }
