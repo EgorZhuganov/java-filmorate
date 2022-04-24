b8a52923c77cb1b1b77910409ffa1fa3a5468c34
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-24 23:10:03 +1000

    change: изменить в связи с добавление проперти UTF-8

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -12,7 +12,7 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/films",  produces = "application/json;charset=UTF-8") //TODO подумать как вынести в properties или настройку проекта
+@RequestMapping("/films")
 @RequiredArgsConstructor
 public class FilmController {
 
