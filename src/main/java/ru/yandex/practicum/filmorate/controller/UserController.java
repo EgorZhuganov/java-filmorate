6568eb1cee9c4da3964ef7af1677b42d57737803
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-25 20:56:48 +1000

    add: добавить проверку ConstraintViolationException, добавить логирование в FilmController, UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -9,11 +10,13 @@ import ru.yandex.practicum.filmorate.dto.userDto.UserReadDto;
 import ru.yandex.practicum.filmorate.dto.userDto.UserUpdateDto;
 import ru.yandex.practicum.filmorate.service.UserService;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -29,29 +32,50 @@ public class UserController {
     public UserReadDto findById(@PathVariable Long id) {
         return userService
                 .findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> {
+                            log.warn("user with id: {} not found", id);
+                            return new ResponseStatusException(HttpStatus.NOT_FOUND);
+                        }
+                );
     }
 
     @PostMapping //201 and return userReadDto
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public UserReadDto create(@RequestBody UserCreateDto user) {
-        return userService.create(user);
+        try {
+            return userService.create(user);
+        } catch (ConstraintViolationException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping //200 OK orElseThrow NOT_FOUND
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public UserReadDto update(@RequestBody UserUpdateDto user) {
-        return userService
-                .update(user.getId(), user)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        try {
+            return userService
+                    .update(user.getId(), user)
+                    .orElseThrow(() -> {
+                                log.warn("user with id: {} not found for update", user.getId());
+                                return new ResponseStatusException(HttpStatus.NOT_FOUND);
+                            }
+                    );
+        } catch (ConstraintViolationException e) {
+            e.printStackTrace();
+            log.warn(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/{id}") //204 no content
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remove(@PathVariable Long id) {
         if (!userService.delete(id)) {
+            log.warn("user with id: {} not found for remove", id);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
