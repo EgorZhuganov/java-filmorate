583ae39b147abfaf90965b72f4bc8a374128a471
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-26 10:38:39 +1000

    add: добавить interface GenerateNextIdService

---
+++ src/main/java/ru/yandex/practicum/filmorate/repository/idGenerator/GenerateNextIdService.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.repository.idGenerator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface GenerateNextIdService<T> {
+
+    T getNextId();
+
+}
