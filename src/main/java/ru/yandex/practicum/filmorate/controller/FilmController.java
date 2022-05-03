3ccfc586684d514e21add68092e70ac7a93b12f6
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-05-03 18:59:57 +1000

    style: удалить лишние аннотации, сделать статический импорт в классе FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.dto.filmDto.FilmCreateDto;
@@ -13,6 +12,8 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 import javax.validation.ConstraintViolationException;
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.*;
+
 @RestController
 @RequestMapping("/films")
 @RequiredArgsConstructor
@@ -22,61 +23,57 @@ public class FilmController {
     private final FilmService filmService;
 
     @GetMapping //200 OK and return List
-    @ResponseBody
     public List<FilmReadDto> findAll() {
         return filmService.findAll();
     }
 
     @GetMapping("/{id}") //200, OK orElseThrow NOT_FOUND
-    @ResponseBody
     public FilmReadDto findById(@PathVariable Long id) {
         return filmService
                 .findById(id)
                 .orElseThrow(() -> {
                             log.warn("film with id: {} not found", id);
-                            return new ResponseStatusException(HttpStatus.NOT_FOUND);
+                            return new ResponseStatusException(NOT_FOUND);
                         }
                 );
     }
 
     @PostMapping //201 and return userReadDto
-    @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody
+    @ResponseStatus(CREATED)
     public FilmReadDto create(@RequestBody FilmCreateDto film) {
         try {
             return filmService.create(film);
         } catch (ConstraintViolationException e) {
             e.printStackTrace();
             log.error(e.getMessage());
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(BAD_REQUEST);
         }
     }
 
     @PutMapping //200 OK orElseThrow NOT_FOUND
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
+    @ResponseStatus(OK)
     public FilmReadDto update(@RequestBody FilmUpdateDto film) {
         try {
             return filmService
                     .update(film.getId(), film)
                     .orElseThrow(() -> {
                                 log.warn("film with id: {} not found for update", film.getId());
-                                return new ResponseStatusException(HttpStatus.NOT_FOUND);
+                                return new ResponseStatusException(NOT_FOUND);
                             }
                     );
         } catch (ConstraintViolationException e) {
             e.printStackTrace();
             log.warn(e.getMessage());
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(BAD_REQUEST);
         }
     }
 
     @DeleteMapping("/{id}") //204 no content
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(NO_CONTENT)
     public void remove(@PathVariable Long id) {
         if (!filmService.delete(id)) {
             log.warn("film with id: {} not found for remove", id);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(NOT_FOUND);
         }
     }
 }
