0e6e368f2723d24e50a7966b5dd01706c7182e25
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-06-03 16:48:16 +1000

    add: добавить сущность MpaRating

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MpaRating implements BaseModel<Long>{
+
+    private Long id;
+    private String name;
+
+}
