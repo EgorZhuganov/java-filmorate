d59005baea85faf3884319b6f9185609df55e3f3
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-06-03 16:57:43 +1000

    add: добавить зависимость на MpaRatingDao

--- src/main/java/ru/yandex/practicum/filmorate/mapper/filmMapper/FilmCreateMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/filmMapper/FilmCreateMapper.java
@@ -1,13 +1,18 @@
 package ru.yandex.practicum.filmorate.mapper.filmMapper;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dto.filmDto.FilmCreateDto;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.repository.database.MpaRatingDao;
 
 import java.util.HashSet;
 
 @Component
+@RequiredArgsConstructor
 public class FilmCreateMapper implements FilmMapper<FilmCreateDto, Film> {
+
+    private final MpaRatingDao mpaRatingDao;
 
     public Film mapFrom(FilmCreateDto object) {
         return Film.builder()
@@ -16,6 +21,7 @@ public class FilmCreateMapper implements FilmMapper<FilmCreateDto, Film> {
                 .releaseDate(object.getReleaseDate())
                 .duration(object.getDuration())
                 .likes(new HashSet<>())
+                .mpaRating(mpaRatingDao.findById(object.getMpaId()))
                 .build();
     }
 
