3e58a5ff0de4d504cf3432242800d09802ff835c
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-06-05 04:20:06 +1000

    feat: добавить FilmRepository

---
+++ src/main/java/ru/yandex/practicum/filmorate/repository/database/abstraction/FilmRepository.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.repository.database.abstraction;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.repository.AbstractRepository;
+
+import java.util.List;
+
+public interface FilmRepository extends AbstractRepository<Long, Film> {
+
+    List<Film> findPopularFilmsByLikes(int count);
+
+    boolean insertLike(Long userId, Long filmId);
+
+    boolean deleteLike(Long userId, Long filmId);
+
+}
