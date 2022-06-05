c535bb14b29bcb65b8ffc4c142b62c9cfc6debee
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-06-06 02:22:51 +1000

    fix: изменить порядок параметров в методах insertLike и deleteLike

--- src/main/java/ru/yandex/practicum/filmorate/repository/database/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/database/FilmRepository.java
@@ -9,8 +9,8 @@ public interface FilmRepository extends AbstractRepository<Long, Film> {
 
     List<Film> findPopularFilmsByLikes(int count);
 
-    boolean insertLike(Long userId, Long filmId);
+    boolean insertLike(Long filmId, Long userId);
 
-    boolean deleteLike(Long userId, Long filmId);
+    boolean deleteLike(Long filmId, Long userId);
 
 }
