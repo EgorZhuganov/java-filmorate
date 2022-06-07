7c9c19a1af29249ad8c185b72670613f38e8536d
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-06-07 14:20:00 +1000

    fix: удалить лишний импорт

--- src/main/java/ru/yandex/practicum/filmorate/dto/filmDto/FilmUpdateDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/filmDto/FilmUpdateDto.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.validation.annotation.IsAfter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.Duration;
 import java.time.LocalDate;
